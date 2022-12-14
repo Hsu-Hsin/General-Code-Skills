67edee9d277f77ad59524a288a96bd9f56ee5596
Hsu-Hsin <Mbrmit@outlook.com>
2022-12-14 13:29:31 +0800

    Create heap.cpp
    An implementation of enhanced Heap(or Priority Queue)

---
+++ heap.cpp
@@ -0,0 +1,94 @@
+/*
+ * store pointer of Node in Heap
+ * -------------------------------------------------
+ * Interface    Fuction
+ * -------------------------------------------------
+ * push()         push Node*
+ * pop()          pop top of Heap
+ * modify()       modify item in the middle of Heap
+ * erase()        erase item in the middle of Heap
+ * clear()        reset Heap
+ * top()          catch top of Heap
+ * empty()        juege if Heap is empty
+ * -------------------------------------------------
+ */
+#define HEAP_SIZE 20000
+
+struct Node {
+    int priority = 0;
+    int idx_heap = 0;
+    /* another way is use hash_map to map node_idx to heap_array_idx */
+};
+
+class Heap {
+   private:
+    Node* arr[HEAP_SIZE] = {nullptr};
+    int sz = 0;
+
+    void heapifyUp(int pos) {
+        while (pos > 0 && compare((pos - 1) >> 1, pos)) {
+            exchange((pos - 1) >> 1, pos);
+            pos = (pos - 1) >> 1;
+        }
+    }
+    void heapifyDown(int pos) {
+        while (pos < sz) {
+            int Lc = (pos << 1) + 1, Rc = (pos << 1) + 2;
+            int idx = pos;
+            if (Lc < sz && compare(idx, Lc)) {
+                idx = Lc;
+            }
+            if (Rc < sz && compare(idx, Rc)) {
+                idx = Rc;
+            }
+            if (idx != pos) {
+                exchange(pos, idx);
+                pos = idx;
+            } else {
+                break;
+            }
+        }
+    }
+    void exchange(int parent, int child) {
+        // exchange content but keep idx
+        Node* tmp = arr[parent];
+        int pidx = arr[parent]->idx_heap, cidx = arr[child]->idx_heap;
+
+        arr[parent] = arr[child];
+        arr[child] = tmp;
+
+        arr[parent]->idx_heap = pidx;
+        arr[child]->idx_heap = cidx;
+    }
+    bool compare(int parent, int child) {
+        return arr[parent]->priority > arr[child]->priority;
+    }
+
+   public:
+    void push(Node* val) {
+        arr[sz] = val;
+        arr[sz]->idx_heap = sz;
+        heapifyUp(sz);
+        ++sz;
+    }
+    void pop() {
+        exchange(0, sz - 1);
+        heapifyDown(0);
+        --sz;
+    }
+    void modify(int pos, Node* val) {
+        arr[pos] = val;
+        heapifyUp(pos);
+        heapifyDown(pos);
+    }
+    void erase(int pos) {
+        exchange(pos, sz - 1);
+        heapifyUp(pos);
+        heapifyDown(pos);
+        --sz;
+    }
+    void clear() { sz = 0; }
+
+    Node* top() const { return arr[0]; }
+    bool empty() const { return sz == 0; }
+};
